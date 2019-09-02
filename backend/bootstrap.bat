092aad7437ed2cd99e01d024b352869991c20ff7
alehechka <42357034+alehechka@users.noreply.github.com>
2019-09-01 23:38:10 -0500

    integrating Flask RESTful endpoints and SQLAlchemy

---
+++ backend/bootstrap.bat
@@ -0,0 +1,3 @@
+SET FLASK_APP="./src/main.py"
+C:\Users\AdamLehechka\.virtualenvs\backend-yult9Ghw\Scripts\activate
+flask run -h 0.0.0.0
---
+++ backend/newExam.bat
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-Type: application/json' -d '{ "title": "TypeScript Advanced Exam", "description": "Tricky questions about TypeScript." }' http://0.0.0.0:5000/exams
+curl http://0.0.0.0:5000/exams
