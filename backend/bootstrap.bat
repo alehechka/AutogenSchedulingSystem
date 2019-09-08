5b6fc9bef447b0064217b864df96ca7459ce5fc3
Adam Lehechka <ajlehechka@gmail.com>
2019-09-08 10:32:08 -0500

    Integrated frontend to work with backend

--- backend/bootstrap.bat
+++ backend/bootstrap.bat
@@ -1,3 +1,7 @@
 SET FLASK_APP="./src/main.py"
-C:\Users\AdamLehechka\.virtualenvs\backend-yult9Ghw\Scripts\activate
-flask run -h 0.0.0.0
+REM pipenv shell
+REM Adam's PC
+REM C:\Users\AdamLehechka\.virtualenvs\backend-yult9Ghw\Scripts\activate
+REM Adam's Surface
+REM C:\Users\ajleh\.virtualenvs\backend-noL0Wlc3\Scripts\activate
+flask run
