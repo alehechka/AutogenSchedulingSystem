aabd3b6c43da2e6d1fae0900fda39ef01f4bf5b9
Adam Lehechka <ajlehechka@gmail.com>
2019-09-09 14:21:09 -0500

    Integrating Angular with Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import * as Auth0 from 'auth0-web';
+import {CallbackComponent} from './callback.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
@@ -5,16 +7,41 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {ExamsApiService} from './exams/exams-api.service';
 
+import {ExamFormComponent} from './exams/exam-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExamsComponent} from './exams/exams.component';
+
+const appRoutes: Routes = [
+  { path: 'new-exam', component: ExamFormComponent },
+  { path: '', component: ExamsComponent },
+  { path: 'callback', component: CallbackComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: 'dev-9r28-4jz.auth0.com',
+      audience: 'https://online-exam.digituz.com.br',
+      clientID: 'Un8JHzEVO5VXfEG4Q15X8OQ3HOnvmeLK',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile manage:exams'
+    });
+  }
 }
