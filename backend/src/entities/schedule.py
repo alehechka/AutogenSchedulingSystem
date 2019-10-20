266dd8ec5d39b0ee2b87e92d15c72c09d40d793b
Adam Lehechka <ajlehechka@gmail.com>
2019-10-20 12:46:27 -0500

    Finished schedule

--- backend/src/entities/schedule.py
+++ backend/src/entities/schedule.py
@@ -60,21 +60,87 @@ def get_schedule_by_employee(employee_id):
     session.close()
     return jsonify(schedule)
 
-@blueprint.route('/add', methods=['POST'], endpoint='add_shift')
+@blueprint.route('/get/position/<position_id>', methods=['GET'])
 @requires_auth
-def add_shift():
+def get_schedule_by_position(position_id):
+    # fetching from the database
+    session = Session()
+    schedule_object = session.query(Schedule).filter(Schedule.position_id == position_id)
+    # transforming into JSON-serializable objects
+    schema = ScheduleSchema(many=True)
+    schedule = schema.dump(schedule_object)
+
+    # serializing as JSON
+    session.close()
+    return jsonify(schedule)
+
+@blueprint.route('/get/department/<department_id>', methods=['GET'])
+@requires_auth
+def get_schedule_by_department(department_id):
+    # fetching from the database
+    session = Session()
+    schedule_object = session.query(Schedule).filter(Schedule.department_id == department_id)
+    # transforming into JSON-serializable objects
+    schema = ScheduleSchema(many=True)
+    schedule = schema.dump(schedule_object)
+
+    # serializing as JSON
+    session.close()
+    return jsonify(schedule)
+
+@blueprint.route('/get/store/<store_id>', methods=['GET'])
+@requires_auth
+def get_schedule_by_store(store_id):
+    # fetching from the database
+    session = Session()
+    schedule_object = session.query(Schedule).filter(Schedule.store_id == store_id)
+    # transforming into JSON-serializable objects
+    schema = ScheduleSchema(many=True)
+    schedule = schema.dump(schedule_object)
+
+    # serializing as JSON
+    session.close()
+    return jsonify(schedule)
+
+@blueprint.route('/add', methods=['POST'], endpoint='add_schedule')
+@requires_auth
+def add_schedule():
     # mount store object
-    posted_shift = ShiftSchema(only=('position_id', 'time_frame_start', 'time_frame_end', 'skill_required', 'people_required', 'day_of_week', 'effective_date', 'expiration_date')) \
+    posted_schedule = ScheduleSchema(only=('position_id', 'store_id', 'department_id', 'employee_id', 'start_date_time', 'end_date_time')) \
         .load(request.get_json())
 
-    shift = Shift(**posted_shift, created_by="HTTP post request")
+    schedule = Schedule(**posted_schedule, created_by="HTTP post request")
 
     # persist store
     session = Session()
-    session.add(shift)
+    session.add(schedule)
     session.commit()
 
     # return created store
-    new_shift = ShiftSchema().dump(shift)
+    new_schedule = ScheduleSchema().dump(schedule)
     session.close()
-    return jsonify(new_shift), 201
+    return jsonify(new_schedule), 201
+
+@blueprint.route('/update/<schedule_id>', methods=['POST'], endpoint='update_schedule')
+@requires_auth
+def update_schedule(schedule_id):
+    # mount store object
+    posted_schedule = ScheduleSchema(only=('position_id', 'store_id', 'department_id', 'employee_id', 'start_date_time', 'end_date_time')) \
+        .load(request.get_json())
+
+    request_schedule = Schedule(**posted_schedule, created_by="HTTP post request")
+
+    # persist store
+    session = Session()
+    schedule = session.query(Schedule).filter(Schedule.id == schedule_id).first()
+    schedule.employee_id = request_schedule.employee_id
+    schedule.start_date_time = request_schedule.start_date_time
+    schedule.end_date_time = request_schedule.end_date_time
+    schedule.last_updated_by = request_schedule.last_updated_by
+    schedule.updated_at = request_schedule.updated_at
+    session.commit()
+
+    # return created store
+    new_schedule = ScheduleSchema().dump(schedule)
+    session.close()
+    return jsonify(new_schedule), 201
